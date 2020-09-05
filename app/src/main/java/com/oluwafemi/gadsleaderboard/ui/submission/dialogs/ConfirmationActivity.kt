d5101d53f006d9390459391e921bf238af9e3bba
dev-juri <ojurifemi132@gmail.com>
2020-09-05 11:55:03 +0100

    Adjustments and Added drawable resources

---
+++ app/src/main/java/com/oluwafemi/gadsleaderboard/ui/submission/dialogs/ConfirmationActivity.kt
@@ -0,0 +1,12 @@
+package com.oluwafemi.gadsleaderboard.ui.submission.dialogs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oluwafemi.gadsleaderboard.R
+
+class ConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation)
+    }
+}
